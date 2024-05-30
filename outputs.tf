e072fd3cba9625518472d5dc2bf451024030c33c
Abigail Ranson <abby@abbyranson.com>
2024-05-29 21:58:29 -0700

    bad commit

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "project_id" {
   value = google_project.project.id
 }
 
+output "project_number" {
+  value = google_project.project.number
+}
+
 output "project" {
   value = local.project_key
 }
